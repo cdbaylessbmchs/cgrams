d59f02a02abffb826842b02254a9cf7b3f6631f1
cdbaylessbmchs <89477868+cdbaylessbmchs@users.noreply.github.com>
2021-08-24 12:29:31 -0500

    Add files via upload

---
+++ gnuexpander.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+//Expands the GNU recursive acronym to i number of times
+
+int main(void){
+  int i;
+  int c;
+  char g[9] = "not Unix ";
+  printf("Input how many GNU expanions you want: \n");
+  scanf( "%d", &i);
+  printf("GNU's ");
+  for (c = i; c > 0; c = c-1){
+	  printf("not Unix ");
+  }
+}
